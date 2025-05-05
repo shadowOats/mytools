8eb9bfc6f808a16af770a28409933fdfb91929b8
Oats <2765653855@qq.com>
2025-05-05 20:11:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# mytools
+
+一个用于简化python开发流程的工具
+
+## 安装
+
+```bash
+pip install git+https://github.com/yourname/mytools.git
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytools',
+    version='0.1.0',
+    description='My personal tools',
+    author='Oats',
+    author_email='1008611@qq.com',
+    packages=find_packages(),  # 自动找到 mytools 包
+    python_requires='>=3.6',
+    install_requires=[],
+)
